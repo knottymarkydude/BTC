d670faca214c120f0d3916ec53acda1926f2f26d
knottymarkydude <mwilson.mark@gmail.com>
2016-04-02 16:37:08 +0100

    2nd April - More membership

--- about/membership.php
+++ about/membership.php
@@ -26,27 +26,42 @@
             <!-- Navigation / End -->
             <!-- Content -->
             <div class="panel panel-body">
-                <h3>Membership</h3>
+                <div class="col-md-8">
 
-                
-                <p>
-                    <img class="img-thumbnail" src="../images/sessions/run/parkrun300.jpg" align="right" alt="Membership">
-                    Belvoir Tri Club take pride in offering an unbelievably low membership fee with amazing benefits worth twice its value. 
-                    Whether you are a Triathlete, Duathlete, Cyclist, Runner, or simply want to train with like-minded individuals, then this is the club for you. If you are interested in membership, please drop an email to <a href="mailto:info@belvoirtriclub.co.uk">info@belvoirtriclub.co.uk</a></p>
-                <p>There are a bunch of member benefits including but not limited to:</p>
-                <ul>
-                    <li>10% off one-to-one training with Tri-SL Training</li>
-                    <li>10% off at Lincolnshire Runner</li>
-                    <li>10% off at CycleSport Grantham</li>
-                    <li>10% off Thomas Cook Charter Packages (<a href="mailto:info@belvoirtriclub.co.uk">email</a> us for details)</li>
-                    <li>25% of all products at www.stoneandwater.co.uk</li>
-                    <li>Free Sunday night when you book Friday and Saturday at www.thornsfarmleisure.co.uk</li>
-                    <li>Discounted bike servicing from Fit2try</li>
-                    <li>Discounted BTF membership</li>
-                    <li>Access to great club kit</li>
-                    <li>The opportunity to compete for the club and win club silverware.</li>
-                </ul>
-
+                    <h3>Membership</h3>
+                    <p>
+                        Belvoir Tri Club take pride in offering an unbelievably low membership fee with amazing benefits worth twice its value. 
+                        Whether you are a Triathlete, Duathlete, Cyclist, Runner, or simply want to train with like-minded individuals, then this is the club for you. 
+                        If you are interested in membership, please drop an email to <a href="mailto:info@belvoirtriclub.co.uk">info@belvoirtriclub.co.uk</a></p>
+                    
+                    <img class="img-thumbnail" src="../images/sessions/run/parkrun300.jpg" align="left" alt="Membership">
+                    <p>There are a bunch of member benefits including but not limited to:</p>
+                    <ul class="list-unstyled">
+                        <li>10% off one-to-one training with Tri-SL Training</li>
+                        <li>10% off at Lincolnshire Runner</li>
+                        <li>10% off at CycleSport Grantham</li>
+                        <li>10% off Thomas Cook Charter Packages (<a href="mailto:info@belvoirtriclub.co.uk">email</a> us for details)</li>
+                        <li>25% of all products at www.stoneandwater.co.uk</li>
+                        <li>Free Sunday night when you book Friday and Saturday at www.thornsfarmleisure.co.uk</li>
+                        <li>Discounted bike servicing from Fit2try</li>
+                        <li>Discounted BTF membership</li>
+                        <li>Access to great club kit</li>
+                        <li>The opportunity to compete for the club and win club silverware.</li>
+                    </ul>
+                    
+                    
+                </div>
+                <div class="col-md-4">
+                    <div class="headline">
+                        <h4>Join the Club</h4>
+                    </div>
+                    <div class="well">
+                        <h2>Membership Options</h2>
+                        <p>You can join as a single sport, dual sport, or multi-sport member. So, for example, if you just want to participate in the swimming sessions,
+                        you can just join as a single sport member.</p>
+                        <button type="button" class="btn btn-inverse">Join Now!</button>
+                    </div>
+                </div>
             </div>
             <!-- Footer   -->
             <?php include '../common/footer.php'; ?>
