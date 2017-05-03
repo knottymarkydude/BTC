6927dbe9007282f69d528cf208b3c4f69f41c75f
mw8 <mw8@mib110553i.internal.sanger.ac.uk>
2017-05-03 10:24:12 +0100

    added go tri results

--- events/gotri/go-tri.php
+++ events/gotri/go-tri.php
@@ -53,7 +53,7 @@
                                             <img class="thumbnail" src="" align="right" alt="">
                                         </p>
                                         <ul id="gotri">
-                                            <li><a href="http://www.entrycentral.com/btc2017aqua1" target="_blank">Saturday 29th April 2017</a>&nbsp;<button id="gotri1" class="btn btn-sm btn-success">Enter</button></li>
+                                            <li><a href="/docs/club/gotri/GoTri20170329.pdf" target="_blank">Saturday 29th April 2017</a>&nbsp;<button id="gotri1" class="btn btn-sm btn-danger">Result</button></li>
                                             <li><a href="http://www.entrycentral.com/btc2017aqua2" target="_blank">Saturday 24th June 2017</a>&nbsp;<button id="gotri2" class="btn btn-sm btn-success">Enter</button></li>
                                             <li><a href="http://www.entrycentral.com/btc2017aqua3" target="_blank">Saturday 29th July 2017</a>&nbsp;<button id="gotri3" class="btn btn-sm btn-success">Enter</button></li>
                                             <li><a href="http://www.entrycentral.com/btc2017aqua4" target="_blank">Saturday 9th September 2017</a>&nbsp;<button id="gotri4" class="btn btn-sm btn-success">Enter</button></li>
@@ -139,6 +139,8 @@
 
                             <div class="tab-pane fade active in" id="results">
                                 <div class="well">
+                                    <p><a href="/docs/club/gotri/GoTri20170329.pdf">Results - 29th April 2017</a></p>
+                                    <hr/>
                                     <p><a href="/docs/club/gotri/April 2nd Results.pdf">Results - 2nd April 2016</a></p>
                                     <p><a href="/docs/club/gotri/May 22nd Results.pdf">Results - 22nd May 2016</a></p>
                                     <p><a href="/docs/club/gotri/July_30th_Results.pdf">Results - 30th July 2016</a></p>
@@ -168,7 +170,7 @@
                 $("#events").addClass("active");
 
                 $('#gotri1').click(function () {
-                    window.location = 'http://www.entrycentral.com/btc2017aqua1';
+                    window.location = '/docs/club/gotri/GoTri20170329.pdf';
                 });
 
                 $('#gotri2').click(function () {
