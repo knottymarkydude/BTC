70f044a4a88dd1d41858f0b5731c7409801760a3
mw8 <mw8@192.168.1.100>
2017-05-16 14:39:48 +0100

    added piwik to site

--- common/js.php
+++ common/js.php
@@ -25,3 +25,18 @@
   ga('send', 'pageview');
 
 </script>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik-piwik-01.1d35.starter-us-east-1.openshiftapps.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
